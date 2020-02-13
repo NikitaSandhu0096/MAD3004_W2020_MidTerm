b0468af9a9bd52f704ef1d3df687d2b3a07c37f4
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-13 17:13:30 -0500

    internet class working

---
+++ C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/Internet.swift
@@ -0,0 +1,15 @@
+//
+//  Internet.swift
+//  C0766552_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Internet : Bill{
+    var providerName : String
+    var internetGBUsed : Int
+    var internetRate : 
+}
