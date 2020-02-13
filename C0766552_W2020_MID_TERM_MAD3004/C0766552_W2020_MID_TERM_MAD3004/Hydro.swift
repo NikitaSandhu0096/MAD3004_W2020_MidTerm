27f6c38ebea60a2aaf342f6a21e31454c77a3d8d
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-13 17:55:54 -0500

    hydro class

---
+++ C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,22 @@
+//
+//  Hydro.swift
+//  C0766552_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Hydro : Bill{
+    var agencyName : String
+    var unitConsumed : Int
+    var hyrdoRate : Int
+    
+    init(billId: Int, billDate: Date, billType: type, totalBillAmount: Double, agencyName : String, unitConsumed : Int, hydroRate : Int) {
+        self.agencyName = agencyName
+        self.unitConsumed = unitConsumed
+        self.hyrdoRate = hydroRate
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: totalBillAmount)
+    }
+}
