38568669b438c4d5a44c7df0f398165820b84844
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-02-15 20:17:59 -0500

    customer object

--- C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/Customer.swift
+++ C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/Customer.swift
@@ -12,25 +12,26 @@ class Customer : IDisplay{
     var customerId : String
     var firstName : String
     var lastName : String
-    var fullName : String{
-        return "\(firstName) \(lastName)"
-    }
+    var fullName : String = ""
     var email : String
     var bills = [String : Bill]()
     var totalAmountToPay : Double = 0.0
     
-    init(customerId : String, firstName : String, lastName : String, fullName : String, email : String, bills : [String : Bill]) {
+    init(customerId : String, firstName : String, lastName : String, email : String) {
         self.customerId = customerId
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
-        self.bills = bills
-        //self.totalAmountToPay = totalAmountToPay
+    }
+    
+    func name() -> String{
+        self.fullName = "\(self.firstName) \(self.lastName)"
+        return self.fullName
     }
     
     func display() {
         print("Customer ID : \(self.customerId)")
-        print("Customer Full Name : \(self.fullName)")
+        print("Customer Full Name : \(name())")
         print("Customer Email ID : \(self.email)")
         print("------Bill Information------")
         print("***************************")
--- C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/main.swift
+++ C0766552_W2020_MID_TERM_MAD3004/C0766552_W2020_MID_TERM_MAD3004/main.swift
@@ -27,3 +27,10 @@ h1.display()
 h2.display()
 m1.display()*/
 
+var c1 = Customer(customerId: "C0001", firstName: "Nikita", lastName: "Sandhu", email: "nsandhu@gmail.com")
+
+var c2 = Customer(customerId: "C0002", firstName: "Kamal", lastName: "Kaur", email: "kkaur@yahoo.com")
+
+/*c1.display()
+c2.display()*/
+
